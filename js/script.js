a2d09bd51b28aa7ff9d7e4ec512b030d01c203c1
yentesparla1 <yentesparla@hotmail.com>
2025-03-13 12:20:36 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -147,3 +147,14 @@
             });
         });
         
+
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const hamburger = document.getElementById('hamburger');
+            const navMenu = document.getElementById('nav-menu');
+        
+            hamburger.addEventListener('click', function() {
+                navMenu.classList.toggle('active');
+                hamburger.classList.toggle('active');
+            });
+        });
